b4e007a4d06a815a4cb62f67df0e3c62603ce73f
seaead <newsaidislast@gmail.com>
2023-12-21 16:49:21 +0500

    modified:   help.html 	new file:   images/hero-image2.png 	modified:   index.html 	new file:   mail.php 	modified:   style.css

---
+++ mail.php
@@ -0,0 +1,170 @@
+<?php
+$postData = $uploadedFile = $statusMsg = '';
+$msgClass = 'errordiv';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $postData = $_POST;
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $receipt = $_POST['receipt'];
+    $statement = $_POST['statement'];
+    $childPhoto = $_POST['childPhoto'];
+    $prescription = $_POST['prescription'];
+    $parentsPasport = $_POST['parentsPasport'];
+     
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($message) && !empty($receipt)
+     && !empty($statement) && !empty($childPhoto) && !empty($prescription)
+     && !empty($parentsPasport)){
+         
+        // Validate email
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+        }else{
+            $uploadStatus = 1;
+             
+            // Upload attachment file
+            if(!empty($_FILES["attachment"]["name"])){
+                 
+                // File path config
+                $targetDir = "uploads/";
+                $fileName = basename($_FILES["attachment"]["name"]);
+                $targetFilePath = $targetDir . $fileName;
+                $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+                 
+                // Allow certain file formats
+                $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg');
+                if(in_array($fileType, $allowTypes)){
+                    // Upload file to the server
+                    if(move_uploaded_file($_FILES["attachment"]["tmp_name"], $targetFilePath)){
+                        $uploadedFile = $targetFilePath;
+                    }else{
+                        $uploadStatus = 0;
+                        $statusMsg = "Sorry, there was an error uploading your file.";
+                    }
+                }else{
+                    $uploadStatus = 0;
+                    $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
+                }
+            }
+             
+            if($uploadStatus == 1){
+                 
+                // Recipient Email, write your email here
+                $toEmail = 'newsaidislast@gmail.com';
+ 
+                // Sender Data
+                $from = $email;
+                $fromName = $name;
+                 
+                // Subject
+                $emailSubject = 'Contact Request Submitted by '.$name;
+                 
+                // Message 
+                $htmlContent = '<h2>Customer Contact Detail!</h2>
+                    <p><b>Name:</b> '.$name.'</p>
+                    <p><b>Email:</b> '.$email.'</p>
+                    <p><b>Message:</b> '.$message.'</p>
+                    <p><b>Расписка:</b> '.$receipt.'</p>
+                    <p><b>Свидетельства рождении ребёнка (копия):</b> '.$statement.'</p>
+                    <p><b>Фото ребёнка:</b> '.$childPhoto.'</p>
+                    <p><b>Рецепт врача:</b> '.$prescription.'</p>
+                    <p><b>Копия паспорта одного из родителей:</b> '.$parentsPasport.'</p>';
+                    
+                 
+                // Header for sender info
+                $headers = "From:". $fromName." <".$from.">";
+ 
+                if(!empty($uploadedFile) && file_exists($uploadedFile)){
+                     
+                    // Boundary 
+                    $semi_rand = md5(time()); 
+                    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+                     
+                    // Headers for attachment 
+                    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+                     
+                    // Multipart boundary 
+                    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+                    "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
+                     
+                    // Preparing attachment
+                    if(is_file($uploadedFile)){
+                        $message .= "--{$mime_boundary}\n";
+                        $fp =    @fopen($uploadedFile,"rb");
+                        $data =  @fread($fp,filesize($uploadedFile));
+                        @fclose($fp);
+                        $data = chunk_split(base64_encode($data));
+                        $message .= "Content-Type: application/octet-stream; name=\"".basename($uploadedFile)."\"\n" . 
+                        "Content-Description: ".basename($uploadedFile)."\n" .
+                        "Content-Disposition: attachment;\n" . " filename=\"".basename($uploadedFile)."\"; size=".filesize($uploadedFile).";\n" . 
+                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                    }
+                     
+                    $message .= "--{$mime_boundary}--";
+                    $returnpath = "-f" . $email;
+                     
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $message, $headers, $returnpath);
+                     
+                    // Delete attachment file from the server
+                    @unlink($uploadedFile);
+                }else{
+                     // Set content-type header for sending HTML email
+                    $headers .= "\r\n". "MIME-Version: 1.0";
+                    $headers .= "\r\n". "Content-type:text/html;charset=UTF-8";
+                     
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $htmlContent, $headers); 
+                }
+                 
+                // If mail sent
+                if($mail){
+                    $statusMsg = 'Your Message was submitted successfully!';
+                    $msgClass = 'succdiv';
+                     
+                    $postData = '';
+                }else{
+                    $statusMsg = 'Your contact request submission failed, please try again.';
+                }
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+    }
+}
+
+
+/*
+$to = "newsaidislast@gmail.com";
+$subject = "New form submission with attachments";
+$message = $_POST["textarea"];
+$from = "yourwebsite@example.com";
+$files = array($_FILES["file1"], $_FILES["file2"], $_FILES["file3"], $_FILES["file4"], $_FILES["file5"]);
+$uid = md5(uniqid(time()));
+$headers = "From: ".$from."\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+$body = "--".$uid."\r\n";
+$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+$body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+$body .= $message."\r\n\r\n";
+foreach($files as $file) {
+    if ($file["error"] == UPLOAD_ERR_OK) {
+        $name = $file["name"];
+        $mime = $file["type"];
+        $content = file_get_contents($file["tmp_name"]);
+        $content = chunk_split(base64_encode($content));
+        $body .= "--".$uid."\r\n";
+        $body .= "Content-Type: ".$mime."; name=\"".$name."\"\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n";
+        $body .= "Content-Disposition: attachment; filename=\"".$name."\"\r\n\r\n";
+        $body .= $content."\r\n\r\n";
+    }
+}
+$body .= "--".$uid."--";
+mail($to, $subject, $body, $headers);
+
+?>
+*/
